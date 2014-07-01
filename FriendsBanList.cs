c46db2b9f604a783afddf940fc6bbe1fd83d7b10
Shaun Lim <shaunblacksheep@gmail.com>
2014-07-01 11:46:03 +0800

    Fixes and added "Block User's Steam Profile"
    - [Fix] NPE when there's no "steamID" element when checking if user is
    banned
    - Added summary for each method
    - Added parameters to block user's profile too.
    - [Fixes] Removed repetitive removeBan in its own method when there is
    no previous file

--- FriendsBanList.cs
+++ FriendsBanList.cs
@@ -18,54 +18,97 @@ namespace SteamBot {
             return true;
         }
 
+        /// <summary>
+        /// List all banned users' Steam64
+        /// </summary>
         public void listAll () {
             if (fileExist()) {
                 XDocument bannedIDs = XDocument.Load(fileName);
-                foreach (var steamid in bannedIDs.Descendants("bannedIDs")) {
-                    Console.WriteLine(TAG + "Banned : " + steamid.Element("steamID").Value.ToString());
+                if (bannedIDs.Descendants("bannedIDs").Elements("steamID").Any()) {
+                    foreach (var steamid in bannedIDs.Descendants("bannedIDs")) {
+                        Console.WriteLine(TAG + " Banned : " + steamid.Element("steamID").Value.ToString());
+                    }
+                } else {
+                    Console.WriteLine(TAG + " : No banned users.");     
                 }
             }
         }
 
+        /// <summary>
+        /// Check if user is banned
+        /// </summary>
+        /// <returns><c>true</c>, if banned was ised, <c>false</c> otherwise.</returns>
+        /// <param name="steam64">Steam64.</param>
         public bool isBanned (ulong steam64) {
             if (fileExist()) {
                 XDocument bannedIDs = XDocument.Load(fileName);
-                foreach (var steamid in bannedIDs.Descendants("bannedIDs")) {
-                    if (steamid.Element("steamID").Value.ToString().Equals(steam64.ToString())) {
-                        return true;
+                if (bannedIDs.Descendants("bannedIDs").Elements("steamID").Any()) {
+                    foreach (var steamid in bannedIDs.Descendants("bannedIDs")) {
+                        if (steamid.Element("steamID").Value.ToString().Equals(steam64.ToString())) {
+                            return true;
+                        }
                     }
                 }
             }
             return false;
         }
 
-        public bool addBan (ulong steam64) {
+        /// <summary>
+        /// <para>Add ban for user</para>
+        /// <para></para>
+        /// <para>Example : ban.addban(Bot, OtherSID, true); -> Add ban to user + block user's Steam profile</para>
+        /// <para>Example : ban.addban(Bot, OtherSID, false); -> Add ban to user ONLY</para>
+        /// </summary>
+        /// <returns><c>true</c>, if ban was added, <c>false</c> otherwise.</returns>
+        /// <param name="bot">Bot.</param>
+        /// <param name="steam64">Steam64.</param>
+        /// <param name="banProfile">If set to <c>true</c> ban user Steam's profile.</param>
+        public bool addBan (Bot bot, ulong steam64, bool banProfile) {
             if (fileExist()) {
-                XDocument doc = XDocument.Load(fileName);
-                XElement service = doc.Element("bannedIDs");
-                service.Add(new XElement ("steamID", steam64.ToString()));
-                doc.Save(fileName);
-                // Console.WriteLine("Ban added for Steam64 :" + steam64.ToString());
+                if (!isBanned(steam64)) { // if does not exist in ban list
+                    XDocument doc = XDocument.Load(fileName);
+                    XElement service = doc.Element("bannedIDs");
+                    service.Add(new XElement ("steamID", steam64.ToString()));
+                    if (banProfile) {
+                        bot.SteamFriends.IgnoreFriend(steam64, true);
+                    }
+                    bot.log.Info(TAG + " [AddBan] : Successful");
+                    doc.Save(fileName);
+                }
             } else {
-                addBan(steam64);
+                addBan(bot, steam64, banProfile);
             }
             return true;
         }
 
-        public bool removeBan (ulong steam64) {
+        /// <summary>
+        /// <para>Remove ban from user</para>
+        /// <para></para>
+        /// <para>Example : ban.removeBan(Bot, OtherSID, true); -> Unban user + unblock user's Steam profile</para>
+        /// <para>Example : ban.removeBan(Bot, OtherSID, false); -> Unban user ONLY</para>
+        /// </summary>
+        /// <returns><c>true</c>, if ban was removed, <c>false</c> otherwise.</returns>
+        /// <param name="bot">Bot.</param>
+        /// <param name="steam64">Steam64.</param>
+        /// <param name="unbanProfile">If set to <c>true</c> unban user Steam's profile.</param>
+        public bool removeBan (Bot bot, ulong steam64, bool unbanProfile) {
             if (fileExist()) {
                 XDocument bannedIDs = XDocument.Load(fileName);
-                foreach (var steamid in bannedIDs.Descendants("bannedIDs")) {
-                    if (steamid.Element("steamID").Value.ToString().Equals(steam64.ToString())) {
-                        bannedIDs.Descendants("bannedIDs")
+                if (bannedIDs.Descendants("bannedIDs").Elements("steamID").Any()) {
+                    foreach (var steamid in bannedIDs.Descendants("bannedIDs")) {
+                        if (steamid.Element("steamID").Value.ToString().Equals(steam64.ToString())) {
+                            bannedIDs.Descendants("bannedIDs")
                         .Elements("steamID")
                         .Where(x => x.Value == steam64.ToString())
                         .Remove();
+                            if (unbanProfile) {
+                                bot.SteamFriends.IgnoreFriend(steam64, false);
+                            }
+                            bot.log.Info(TAG + " [RemoveBan] : Successful");
+                        }
                     }
                 }
                 bannedIDs.Save(fileName);
-            } else {
-                removeBan(steam64);
             }
             return true;
         }
--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 SteamBotBanList
 =========
-
 A SteamBot extension for managing bans.
 
 Installation
 ----
 Download `FriendsBanList.cs` and import it into your `SteamBot` project
-
 
 Usage
 -----------
@@ -18,17 +16,19 @@ Methods
 
 `bool isBanned(ulong steam64)` - Check if Steam64 ID is banned
 
-`bool addban(ulong steam64)` - Add ban for user
+`bool addBan(Bot bot, ulong steam64, bool banProfile)` - Add ban for user
 
-`bool removeban(ulong steam64)` - Remove ban for user
+`bool removeBan(Bot bot, ulong steam64, bool unbanProfile)` - Remove ban for user
 
 Example
 --------------
 ```C#
         public override bool OnFriendAdd () {
             if (!ban.isBanned(OtherSID)) {
-                Bot.log.Success(Bot.SteamFriends.GetFriendPersonaName(OtherSID) + " (" + OtherSID.ToString() + ") added me!");
+                Bot.log.Success("User added me");
                 return true;
+            } else {
+                ban.addBan(Bot, OtherSID, true); // add to ban list + block user profile
             }
             return false;
         }```
